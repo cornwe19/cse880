c23e822cf1792296c8802db5d2f5319d4a7fe2d7
Dennis Cornwell <cornwe19@gmail.com>
2012-10-02 00:11:50 -0400

    Updating homework 2 answers with the ridiculous answer for question 3.

--- HW2/homework2.sql
+++ HW2/homework2.sql
@@ -58,3 +58,155 @@ BEGIN
     UPDATE Players SET Players.avg_score=0 WHERE Players.eid=OmittedPlayer.pid;
   END LOOP;
 END;
+
+'''
+Question 3
+ - Note: Assumes that Stadiums represent the location earning money from a sale.
+'''
+DROP TABLE Sales CASCADE CONSTRAINTS;
+CREATE TABLE Sales(
+  sale_id NUMBER PRIMARY KEY,
+  gid NUMBER NOT NULL,
+  amount NUMBER,
+  FOREIGN KEY(gid) REFERENCES Games(gid)
+);
+
+/* LOCATION 1 */
+DROP TABLE Sales_Loc_1 CASCADE CONSTRAINTS;
+CREATE TABLE Sales_Loc_1(
+  gid NUMBER,
+  average_sale NUMBER,
+  total_sale NUMBER,
+  num_transactions NUMBER
+);
+
+CREATE OR REPLACE TRIGGER UPDATE_SALES_LOC_1 
+AFTER INSERT OR DELETE OR UPDATE ON SALES 
+DECLARE
+  CURSOR Sales_Loc_1_Cursor IS
+  SELECT S.gid AS gid, 
+         AVG( S.amount ) AS average_sale, 
+         SUM( S.amount ) AS total_sale, 
+         COUNT(*) AS num_transactions
+  FROM Sales S, Games
+  WHERE S.gid=Games.gid AND
+        Games.sid=1
+  GROUP BY S.gid;
+BEGIN
+  FOR Sales_Stat IN Sales_Loc_1_Cursor LOOP
+    UPDATE Sales_Loc_1 
+    SET average_sale=Sales_Stat.average_sale, 
+        total_sale=Sales_Stat.total_sale, 
+        num_transactions=Sales_Stat.num_transactions
+    WHERE gid=Sales_Stat.gid;
+    IF ( sql%rowcount = 0 ) THEN
+      INSERT INTO Sales_Loc_1 
+      VALUES ( Sales_Stat.gid, Sales_Stat.average_sale, Sales_Stat.total_sale, Sales_Stat.num_transactions );
+    END IF;
+  END LOOP;
+END;
+
+/* LOCATION 2 */
+DROP TABLE Sales_Loc_2 CASCADE CONSTRAINTS;
+CREATE TABLE Sales_Loc_2(
+  gid NUMBER,
+  average_sale NUMBER,
+  total_sale NUMBER,
+  num_transactions NUMBER
+);
+
+CREATE OR REPLACE TRIGGER UPDATE_SALES_LOC_2 
+AFTER INSERT OR DELETE OR UPDATE ON SALES 
+DECLARE
+  CURSOR Sales_Loc_2_Cursor IS
+  SELECT S.gid AS gid, 
+         AVG( S.amount ) AS average_sale, 
+         SUM( S.amount ) AS total_sale, 
+         COUNT(*) AS num_transactions
+  FROM Sales S, Games
+  WHERE S.gid=Games.gid AND
+        Games.sid=2
+  GROUP BY S.gid;
+BEGIN
+  FOR Sales_Stat IN Sales_Loc_2_Cursor LOOP
+    UPDATE Sales_Loc_2 
+    SET average_sale=Sales_Stat.average_sale, 
+        total_sale=Sales_Stat.total_sale, 
+        num_transactions=Sales_Stat.num_transactions
+    WHERE gid=Sales_Stat.gid;
+    IF ( sql%rowcount = 0 ) THEN
+      INSERT INTO Sales_Loc_2 
+      VALUES ( Sales_Stat.gid, Sales_Stat.average_sale, Sales_Stat.total_sale, Sales_Stat.num_transactions );
+    END IF;
+  END LOOP;
+END;
+
+/* LOCATION 3 */
+DROP TABLE Sales_Loc_3 CASCADE CONSTRAINTS;
+CREATE TABLE Sales_Loc_3(
+  gid NUMBER,
+  average_sale NUMBER,
+  total_sale NUMBER,
+  num_transactions NUMBER
+);
+
+CREATE OR REPLACE TRIGGER UPDATE_SALES_LOC_3 
+AFTER INSERT OR DELETE OR UPDATE ON SALES 
+DECLARE
+  CURSOR Sales_Loc_3_Cursor IS
+  SELECT S.gid AS gid, 
+         AVG( S.amount ) AS average_sale, 
+         SUM( S.amount ) AS total_sale, 
+         COUNT(*) AS num_transactions
+  FROM Sales S, Games
+  WHERE S.gid=Games.gid AND
+        Games.sid=3
+  GROUP BY S.gid;
+BEGIN
+  FOR Sales_Stat IN Sales_Loc_3_Cursor LOOP
+    UPDATE Sales_Loc_3 
+    SET average_sale=Sales_Stat.average_sale, 
+        total_sale=Sales_Stat.total_sale, 
+        num_transactions=Sales_Stat.num_transactions
+    WHERE gid=Sales_Stat.gid;
+    IF ( sql%rowcount = 0 ) THEN
+      INSERT INTO Sales_Loc_3 
+      VALUES ( Sales_Stat.gid, Sales_Stat.average_sale, Sales_Stat.total_sale, Sales_Stat.num_transactions );
+    END IF;
+  END LOOP;
+END;
+
+/* LOCATION 4 */
+DROP TABLE Sales_Loc_4 CASCADE CONSTRAINTS;
+CREATE TABLE Sales_Loc_4(
+  gid NUMBER,
+  average_sale NUMBER,
+  total_sale NUMBER,
+  num_transactions NUMBER
+);
+
+CREATE OR REPLACE TRIGGER UPDATE_SALES_LOC_4 
+AFTER INSERT OR DELETE OR UPDATE ON SALES 
+DECLARE
+  CURSOR Sales_Loc_4_Cursor IS
+  SELECT S.gid AS gid, 
+         AVG( S.amount ) AS average_sale, 
+         SUM( S.amount ) AS total_sale, 
+         COUNT(*) AS num_transactions
+  FROM Sales S, Games
+  WHERE S.gid=Games.gid AND
+        Games.sid=4
+  GROUP BY S.gid;
+BEGIN
+  FOR Sales_Stat IN Sales_Loc_4_Cursor LOOP
+    UPDATE Sales_Loc_4 
+    SET average_sale=Sales_Stat.average_sale, 
+        total_sale=Sales_Stat.total_sale, 
+        num_transactions=Sales_Stat.num_transactions
+    WHERE gid=Sales_Stat.gid;
+    IF ( sql%rowcount = 0 ) THEN
+      INSERT INTO Sales_Loc_4 
+      VALUES ( Sales_Stat.gid, Sales_Stat.average_sale, Sales_Stat.total_sale, Sales_Stat.num_transactions );
+    END IF;
+  END LOOP;
+END;
